de4962b72d5212d304980249cdbff0097cb09cfe
Alex Fish <fish@ustwo.co.uk>
2012-08-08 16:38:19 +0100

    Updating stringsFromText: so that it functions without the need for spaces and implementing vertical centre alignment.
    * Updating drawTextInRect to align text vertical centre.
    * TODO add functionality to align by top/middle/bottom.

--- MSLabel.m
+++ MSLabel.m
@@ -32,24 +32,35 @@
     NSArray *slicedStrings = [self stringsFromText:self.text];
     [self.textColor set];
     
-    for (int i = 0; i < slicedStrings.count; i++) {
-        if (i + 1 > self.numberOfLines && self.numberOfLines != 0)
-            break;
-        
+    int numLines = slicedStrings.count;
+    if (numLines > self.numberOfLines && self.numberOfLines != 0)
+    {
+        numLines = self.numberOfLines;
+    }
+    int drawHeight = self.frame.size.height / 2 - (_lineHeight * numLines) / 2;
+    int drawPos = drawHeight;
+    for (int i = 0; i < numLines; i++) {        
         NSString *line = [slicedStrings objectAtIndex:i];
         
         // calculate drawHeight based on anchor
-        int drawHeight = _anchorBottom ? (self.frame.size.height - (slicedStrings.count - i) * _lineHeight) : i * _lineHeight;        
+        //int drawHeight = ((self.frame.size.height - ((slicedStrings.count - i) * _lineHeight) ) / 2 ); 
+        
+        if(i > 0)
+        {
+            drawPos += _lineHeight;            
+        }
+        
+        //drawHeight = drawHeight + _lineHeight * slicedStrings.count - i;
         
         // calculate drawWidth based on textAlignment
-        int drawWidth = 0;
+        int drawWidth = 0; 
         if (self.textAlignment == UITextAlignmentCenter) {
             drawWidth = floorf((self.frame.size.width - [line sizeWithFont:self.font].width) / 2);
         } else if (self.textAlignment == UITextAlignmentRight) {
             drawWidth = (self.frame.size.width - [line sizeWithFont:self.font].width);
         }
         
-        [line drawAtPoint:CGPointMake(drawWidth, drawHeight) forWidth:self.frame.size.width withFont:self.font fontSize:self.font.pointSize lineBreakMode:UILineBreakModeClip baselineAdjustment:UIBaselineAdjustmentNone];
+        [line drawAtPoint:CGPointMake(drawWidth, drawPos) forWidth:self.frame.size.width withFont:self.font fontSize:self.font.pointSize lineBreakMode:UILineBreakModeClip baselineAdjustment:UIBaselineAdjustmentNone];
     }
 }
 
@@ -63,33 +74,52 @@
 
 #pragma mark - Private Methods
 
-- (NSArray *)stringsFromText:(NSString *)string {
-    NSMutableArray *stringsArray = [[[string componentsSeparatedByString:@" "] mutableCopy] autorelease];
+- (NSArray *)stringsFromText:(NSString *)string 
+{
+    
+    NSRange theRange = {0, 1};
+    
+    NSMutableArray *stringsArray  = [NSMutableArray array];
+    
+    for ( NSInteger i = 0; i < [string length]; i++) 
+    {
+        theRange.location = i;
+        [stringsArray addObject:[string substringWithRange:theRange]];
+    }
+    
     NSMutableArray *slicedString = [NSMutableArray array];
     
-    while (stringsArray.count != 0) {
+    while (stringsArray.count != 0) 
+    {
         NSString *line = [NSString stringWithString:@""];
         NSMutableIndexSet *wordsToRemove = [NSMutableIndexSet indexSet];
         
-        for (int i = 0; i < [stringsArray count]; i++) {
+        for (int i = 0; i < [stringsArray count]; i++) 
+        {
             NSString *word = [stringsArray objectAtIndex:i];
             
-            if ([[line stringByAppendingFormat:@"%@ ", word] sizeWithFont:self.font].width <= self.frame.size.width) {
-                line = [line stringByAppendingFormat:@"%@ ", word];
+            if ([[line stringByAppendingFormat:@"%@", word] sizeWithFont:self.font].width <= self.frame.size.width) 
+            {
+                line = [line stringByAppendingFormat:@"%@", word];
                 [wordsToRemove addIndex:i];
-            } else {
-                if (line.length == 0) {
-                    line = [line stringByAppendingFormat:@"%@ ", word];
+            } 
+            else 
+            {
+                if (line.length == 0) 
+                {
+                    line = [line stringByAppendingFormat:@"%@", word];
                     [wordsToRemove addIndex:i];
                 }
                 break;
             }
         }
+        
         [slicedString addObject:[line stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]]];
         [stringsArray removeObjectsAtIndexes:wordsToRemove];
     }
     
-    if (slicedString.count > self.numberOfLines && self.numberOfLines != 0) {
+    if (slicedString.count > self.numberOfLines && self.numberOfLines != 0) 
+    {
         NSString *line = [slicedString objectAtIndex:(self.numberOfLines - 1)];
         line = [line stringByReplacingCharactersInRange:NSMakeRange(line.length - 3, 3) withString:@"..."];
         [slicedString removeObjectAtIndex:(self.numberOfLines - 1)];
